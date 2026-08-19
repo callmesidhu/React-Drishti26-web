422a779f695fa22e38598f29a056cbebd4df6d6b
rohitmendus <rohitmendus@gmail.com>
2026-08-19 12:08:29 +0530

    feat(server): added points updation endpoint

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import AmbassadorLeaderboardAPIView
+from .views import AmbassadorLeaderboardAPIView, AddPointsLeaderboardAPIView
 
 urlpatterns = [
     path('api/ambassador/leaderboard/', AmbassadorLeaderboardAPIView.as_view()),
-    # path('api/ambassador/add_points/', admin.site.urls),
+    path('api/ambassador/add_points/', AddPointsLeaderboardAPIView.as_view()),
     # path('api/ambassador/add', admin.site.urls),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,12 @@
 from rest_framework.views import APIView
+from rest_framework_api_key.permissions import HasAPIKey
 from rest_framework.response import Response
+from rest_framework import status
+
 from .models import CampusAmbassador
+
+from django.shortcuts import get_object_or_404
+from django.db.models import F
 
 
 # Create your views here.
@@ -18,3 +24,34 @@ class AmbassadorLeaderboardAPIView(APIView):
             })
 
         return Response(data)
+    
+
+class AddPointsLeaderboardAPIView(APIView):
+    permission_classes=[HasAPIKey]
+    
+    def post(self, request):
+        referral_code = request.data.get('referral_code')
+        if not referral_code:
+            return Response(
+                {"error": "referral_code is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        ambassador = get_object_or_404(CampusAmbassador, referral_code=referral_code)
+        CampusAmbassador.objects.filter(
+            id=ambassador.id
+        ).update(
+            points=F("points") + 1
+        )
+        
+        ambassador.refresh_from_db()
+        
+        return Response(
+            {
+                "message": "Point added successfully",
+                "referral_code": ambassador.referral_code,
+                "points": ambassador.points,
+            },
+            status=status.HTTP_200_OK
+        )
+
+        
