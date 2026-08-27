9a09851839c4e68f5a2d4830f58a5cf73829bdc7
vs-tries-to-code <vishnusree1805@gmail.com>
2026-08-27 23:56:11 +0530

    Modified routing for homepage display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import Team from './pages/Team.jsx'
 import Competitions from './pages/Competitions.jsx'
 import Workshops from './pages/Workshops.jsx'
 import About from './pages/About.jsx'
+import Home from './pages/Home.jsx'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/daksha" replace />} />
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/daksha" element={<Daksha />} />
       <Route path="/team" element={<Team />} />
       <Route path="/competitions" element={<Competitions />} />
       <Route path="/workshops" element={<Workshops />} />
       <Route path="/about" element={<About />} />
-      <Route path="*" element={<Navigate to="/daksha" replace />} />
+      <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   )
 }
