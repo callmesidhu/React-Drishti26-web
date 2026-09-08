4cb8a295a78d9d86ab311fcd4bbdb941ac582a8e
rohitmendus <rohitmendus@gmail.com>
2026-09-08 21:30:08 +0530

    Congured django static, media files

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import CampusAmbassador
+from .models import Event, EventLink
 
 # Register your models here.
 admin.site.register(CampusAmbassador)
+admin.site.register(Event)
+admin.site.register(EventLink)
--- server/server/settings.py
+++ server/server/settings.py
@@ -40,6 +40,12 @@ ALLOWED_HOSTS = [
     if host.strip()
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', '').split(',')
+    if origin.strip()
+]
+
 
 # Application definition
 
@@ -130,7 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Email
