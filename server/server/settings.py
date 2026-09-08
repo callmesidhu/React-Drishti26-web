39bec991bc22ae8257ab08eb2db87e2b7f0c32ba
rohitmendus <rohitmendus@gmail.com>
2026-09-08 19:45:31 +0530

    Added event models

--- server/server/settings.py
+++ server/server/settings.py
@@ -32,7 +32,7 @@ load_dotenv(PROJECT_ROOT / '.env')
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DJANGO_DEBUG']
+DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     host.strip()
